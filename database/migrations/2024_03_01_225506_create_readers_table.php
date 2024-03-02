702d5ad8c8bb2919ba7fa3afafd747013208e801
Marcelo Lipienski <marcelo.lipienski@gmail.com>
2024-03-02 18:43:21 -0300

    Fix typo on readers' table name

--- database/migrations/2024_03_01_225506_create_readers_table.php
+++ database/migrations/2024_03_01_225506_create_readers_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('rearders', function (Blueprint $table) {
+        Schema::create('readers', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('rearders');
+        Schema::dropIfExists('readers');
     }
 };
