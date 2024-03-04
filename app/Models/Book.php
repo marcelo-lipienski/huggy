7c04174b01c02c39fa20a69e8d1251f6faa0cdf4
Marcelo Lipienski <marcelo.lipienski@gmail.com>
2024-03-03 21:24:47 -0300

    Add many to many relationship between readers and books

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Book extends Model
 {
@@ -29,5 +30,13 @@ class Book extends Model
     public function publisher(): BelongsTo
     {
         return $this->belongsTo(Publisher::class, 'publisher_id');
+    }
+
+    /**
+     * @return BelongsToMany<Reader>
+     */
+    public function readers(): BelongsToMany
+    {
+        return $this->belongsToMany(Reader::class, 'readers_books');
     }
 }
